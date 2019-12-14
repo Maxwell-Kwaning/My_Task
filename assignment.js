8d37863af7178ce900fc163e33fb3bc028f4c2b4
Maxwell-Kwaning <56012834+Maxwell-Kwaning@users.noreply.github.com>
2019-12-13 19:00:11 -0800

    Create assignment.js

---
+++ assignment.js
@@ -0,0 +1,69 @@
+console.log(assignment...);
+
+var
+let
+const
+
+var x=2;
+var z=10;
+let name= "assignment";
+
+let dit=['josephine', 'Mummy','Max', 'Daniel' ]
+var number = [1,2,3,4,5,6,7,8,9]
+
+let person1={
+    
+name: 'Daniel yeboah',
+heigth: 16.89,
+color: 'Dark'
+
+
+} 
+let person2={
+
+name: 'maxxy',
+heigth: 14.89,
+color: 'Fair'
+    
+}
+
+// for loop
+
+for(let i=0; i<10 i++){
+
+    console.log("hello world")
+    document.writeln("hello world")
+}
+
+// if statement likely examination question
+var x=89;
+var y=90;
+if(x<y){
+    document.write('x is less than y')
+}else{
+    document.write("oh x is not less than y")
+    }
+
+
+
+
+    let mark=70;
+
+
+    if(marks >=80 && marks<=100){
+        document.write("A")
+    }else if(marks >=60 && marks<=10){
+
+        document.write("F")
+
+    }else if(marks >=60 && marks<=10){
+
+        document.write("F")
+
+    }else if(marks >=60 && marks==10){
+
+        document.write("F")
+
+    }else{
+        document.write("your grade is poor")
+    }
